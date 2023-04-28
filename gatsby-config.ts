a24a4510b55093861512713a0da5770bd39770e9
Alistair.Bancroft <alistair@0.7734.co.uk>
2023-04-28 15:43:21 +0100

    feat(font): add google font

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -91,5 +91,14 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `Roboto\:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700`
+        ],
+        display: 'swap'
+      }
+    }
   ],
 }
