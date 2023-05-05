b3d7458a1731cb36d5bb9d337f8af81ff205cbcd
Anil Maharjan <1720245+dejavu1987@users.noreply.github.com>
2023-05-05 17:57:49 +0200

    Home / Banner and services section styling

--- src/components/HeroBanner.tsx
+++ src/components/HeroBanner.tsx
@@ -1,13 +1,13 @@
 import { Link } from "gatsby"
 import React from "react"
-import { Banner } from "../typescript/component";
+import { Banner } from "../typescript/component"
 
 type Data = {
-  hero_banner: Banner;
+  hero_banner: Banner
 }
 
 type BannerProps = {
-  data: Data;
+  data: Data
 }
 
 const HeroBanner = (props: BannerProps) => {
@@ -17,39 +17,42 @@ const HeroBanner = (props: BannerProps) => {
       className="hero-banner"
       style={{
         backgroundColor: hero_banner.bg_color ? hero_banner.bg_color : "",
-        backgroundImage: hero_banner.banner_image ? `url(${hero_banner.banner_image.url})` : '',
+        backgroundImage: hero_banner.banner_image
+          ? `url(${hero_banner.banner_image.url})`
+          : "",
       }}
     >
       <div className="wrapper">
-      <div className="home-content" style={{ color: hero_banner.text_color }}>
-        {hero_banner.banner_title && (
-          <h1 className="hero-title" {...hero_banner.$?.banner_title}>
-            {hero_banner.banner_title}
-          </h1>
-        )}
-        {hero_banner.banner_description ? (
-          <p
-            className="hero-description"
-            style={{ color: hero_banner.text_color }}
-            {...hero_banner.$?.banner_description}
-          >
-            {hero_banner.banner_description}
-          </p>
-        ) : (
-          ""
-        )}
-        {hero_banner.call_to_action.title && hero_banner.call_to_action.href ? (
-          <Link
-            to={hero_banner.call_to_action.href}
-            className="btn primary-btn"
-            {...hero_banner.call_to_action.$?.title}
-          >
-            {hero_banner.call_to_action.title}
-          </Link>
-        ) : (
-          ""
-        )}
-      </div>
+        <div className="hero-inner" style={{ color: hero_banner.text_color }}>
+          {hero_banner.banner_title && (
+            <h1 className="hero-title" {...hero_banner.$?.banner_title}>
+              {hero_banner.banner_title}
+            </h1>
+          )}
+          {hero_banner.banner_description ? (
+            <p
+              className="hero-description"
+              style={{ color: hero_banner.text_color }}
+              {...hero_banner.$?.banner_description}
+            >
+              {hero_banner.banner_description}
+            </p>
+          ) : (
+            ""
+          )}
+          {hero_banner.call_to_action.title &&
+          hero_banner.call_to_action.href ? (
+            <Link
+              to={hero_banner.call_to_action.href}
+              className="btn primary-btn"
+              {...hero_banner.call_to_action.$?.title}
+            >
+              {hero_banner.call_to_action.title}
+            </Link>
+          ) : (
+            ""
+          )}
+        </div>
       </div>
     </div>
   )
