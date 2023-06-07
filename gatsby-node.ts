5d7d00e7a0d72ce5beaff8f45a3ad4ebccdbfed8
Anil Maharjan <1720245+dejavu1987@users.noreply.github.com>
2023-06-07 12:43:42 +0200

    clean up

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -2,18 +2,8 @@ const path = require("path")
 
 module.exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
-  const blogPostTemplate = path.resolve("src/templates/blog-post.tsx")
+
   const pageTemplate = path.resolve("src/templates/page.tsx")
-  const blogPostQuery = await graphql(`
-    query {
-      allContentstackBlogPost {
-        nodes {
-          title
-          url
-        }
-      }
-    }
-  `)
 
   const pageQuery = await graphql(`
     query {
@@ -26,16 +16,6 @@ module.exports.createPages = async ({ graphql, actions }) => {
     }
   `)
 
-  const createBlogPostTemplate = (route, comp, title) => {
-    createPage({
-      path: `${route}`,
-      component: comp,
-      context: {
-        title: title,
-      },
-    })
-  }
-
   const createPageTemplate = (route, comp, url) => {
     createPage({
       path: `${route}`,
@@ -45,9 +25,6 @@ module.exports.createPages = async ({ graphql, actions }) => {
       },
     })
   }
-  blogPostQuery.data.allContentstackBlogPost.nodes.forEach(node => {
-    createBlogPostTemplate(node.url, blogPostTemplate, node.title)
-  })
   pageQuery.data.allContentstackPage.nodes.forEach(node => {
     if (node.url !== "/" && node.url !== "/blog") {
       createPageTemplate(node.url, pageTemplate, node.url)
